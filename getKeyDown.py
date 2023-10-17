1d484cb6f084c8b3bfc469339ae1daad1909b765
ehnaaaaaa <llianellpp.pp@gmail.com>
2023-10-17 20:50:04 +0300

    GKD

--- getKeyDown.py
+++ getKeyDown.py
@@ -1,20 +1,37 @@
-from pynput import mouse, keyboard
-from threading import Thread
+from multiprocessing import Process
 
+import keyboard
+import mouse
+from pynput import mouse
 
-def KeyboardEvent():
-    with keyboard.Events() as keyboardEvents:
-        for event in keyboardEvents:
-            print(event.key)
+inputFile = open("input.txt", "w")
 
 
 def on_click(x, y, button, pressed):
     if pressed:
+        inputFile.write(str(button))
         print(button, [x, y])
 
 
-keyboardThread = Thread(target=KeyboardEvent)
-keyboardThread.start()
+def on_key_press(event):
+    inputFile.write(event.name)
+    print(event.name)
 
-with mouse.Listener(on_click=on_click) as mouseListener:
-    mouseListener.join()
+
+keyboard.on_press(on_key_press)
+
+
+def start_listener():
+    listener = mouse.Listener(on_click=on_click, keyboard_click=on_key_press)
+    listener.start()
+    listener.join()
+
+
+if __name__ == "__main__":
+    p = Process(target=start_listener)
+    p.start()
+    while True:
+        if keyboard.is_pressed('q'):
+            inputFile.close()
+            break
+    p.terminate()
