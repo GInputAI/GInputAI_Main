c5a8567f9515912428064341aecb44ab61760705
ehnaaaaaa <llianellpp.pp@gmail.com>
2023-10-17 19:22:00 +0300

    New get key down

--- getKeyDown.py
+++ getKeyDown.py
@@ -1,13 +1,21 @@
-import pynput.mouse
-from pynput import keyboard
-
-with keyboard.Events() as keyboardEvents:
-    for keyboardEvent in keyboardEvents:
-        print(keyboardEvent)
+from pynput import keyboard, mouse
+from threading import Thread
 
 
-with pynput.mouse.Events() as mouseEvents:
-    for mouseEvent in mouseEvents:
-        print(mouseEvent)
+def KeyboardEvent():
+    with keyboard.Events() as keyboardEvents:
+        for keyboardEvent in keyboardEvents:
+            print(keyboardEvent)
 
+
+def MouseEvent():
+    with mouse.Events() as events:
+        for event in events:
+            print(event)
+
+
+thread1 = Thread(target=KeyboardEvent)
+thread2 = Thread(target=MouseEvent)
+thread1.start()
+thread2.start()
 
