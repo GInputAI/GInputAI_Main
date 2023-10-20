8aa5a1206e4c7ca5c6fcd5c59b09ed2f041056ba
OpexHunter <chaserhuntcubix@gmail.com>
2023-10-20 23:46:48 +0300

    Test

--- exec/InputWork.py
+++ exec/InputWork.py
@@ -1,49 +1,7 @@
-import pynput
-from pynput.mouse import Button, Controller as ControllerM
-from pynput.keyboard import Controller as ControllerK
-import time
 import pickle
 
+with open('../readers/read_script.pickle', "rb") as file:
+    inputs = pickle.load(file)
 
-mouse = ControllerM()
-keyboard = ControllerK()
-
-MouseListButton = {'Button.left': Button.left, 'Button.right': Button.right}
-KeyboardList = {'<Key.shift: <160>>': 'Shift'}
-
-
-def click(x, y, Button):
-    mouse.position = (x, y)
-    mouse.click(Button)
-
-def InputM(x, y, Button, Delay):
-    time.sleep(Delay)
-    click(x, y, Button)
-def InputK(Button, Delay):
-    time.sleep(Delay)
-    #print(Button)
-    keyboard.press(Button)
-    keyboard.release(Button)
-
-class InputWork():
-    inputs = []
-    def __init__(self, FilePatch):
-        # """Read .pickle
-        with open('../readers/read_script.pickle', "rb") as file:
-            self.inputs = pickle.load(file)
-        # """
-
-        """Read .txt
-        with open(FilePatch, 'r') as f:
-            for i in f:
-                self.inputs.append(eval(i))
-                """
-    def Exec(self):
-        for i in self.inputs:
-            if len(i) == 4:
-                InputM(i[0], i[1], i[2], i[3])
-            else:
-                InputK(i[0], i[1])
-
-read_script1 = InputWork('../readers/read_script.pickle')
-read_script1.Exec()
+for i in inputs:
+    i()
--- exec/getKeyDown.py
+++ exec/getKeyDown.py
@@ -1,54 +1,128 @@
 import time
 from multiprocessing import Process, Manager
-import keyboard
+import keyboard as kb
 from pynput import mouse
-from pynput import keyboard as keyboard_event
+from pynput.mouse import Controller as ControllerM
+from pynput import keyboard
+from pynput.keyboard import Controller as ControllerK
 from copy import deepcopy
 import pickle
 
-def KeyboardEvent(reader, time_start):
-    with keyboard_event.Events() as keyboardEvents:
-        for event in keyboardEvents:
-            reader.append([event.key, time.time() - time_start])
+mouse_controller = ControllerM()
+keyboard_controller = ControllerK()
 
-def on_click(x, y, button, pressed, reader, time_start):
-    if pressed:
-        reader.append([x, y, button, time.time() - time_start])
+class Reformat():
+    def click_press(self, Button, Delay):
+        time.sleep(Delay)
+        mouse_controller.press(Button)
+
+    def click_release(self, Button, Delay):
+        time.sleep(Delay)
+        mouse_controller.release(Button)
+    def re_type_click(self, ButtonM, Delay, **kwargs):
+        if True:
+            return lambda: self.click_press(ButtonM , Delay)
+        else:
+            return lambda: self.click_release(ButtonM, Delay)
+
+    def on_move(self, x, y, Delay, **kwargs):
+        time.sleep(Delay)
+        mouse.position = (x, y)
+
+    def on_press(self, ButtonK, Delay, **kwargs):
+        time.sleep(Delay)
+        keyboard_controller.press(ButtonK)
+
+    def on_release(self, ButtonK, Delay, **kwargs):
+        time.sleep(Delay)
+        keyboard_controller.release(ButtonK)
 
 
-def start_listener(reader, time_start):
-    with mouse.Listener(on_click=lambda x, y, b, p: on_click(x, y, b, p, reader, time_start)) as listener:
-        listener.join()
+    FuncList = {'on_click': re_type_click, 'on_move': on_move, 'on_press': on_press, 'on_release': on_release}
+
+    def Txt_to_Func(self):
+        for i in range(len(self.reader)):
+            self.reader[i] = lambda: self.FuncList[self.reader[i][0]](ButtonM = self.reader[i][-3],
+                                                                      Delay = self.reader[i][-1], x = self.reader[i][1], y = self.reader[i][2],
+                                                                      ButtonK = self.reader[i][1])
+    def Save_to_file(self):
+        with open("../readers/read_script.pickle", "wb") as file:
+            pickle.dump(reader, file)
+            file.close()
+    def __init__(self, reader, time_start):
+        self.reader = list(reader[:])
+        for i in range(len(self.reader)):
+            self.reader[i][-1] += - time_start
+
+        temp_reader = deepcopy(self.reader)
+        for i in range(1, len(reader)):
+            self.reader[i][-1] = self.reader[i][-1] - temp_reader[i - 1][-1]
+
+        '''
+        print(list(filter(lambda x: x[0] == 'on_click',self.reader))[0])
+        print(list(filter(lambda x: x[0] == 'on_move', self.reader))[0])
+        print(list(filter(lambda x: x[0] == 'on_press', self.reader))[0])
+        print(list(filter(lambda x: x[0] == 'on_release', self.reader))[0])
+        '''
+
+        self.Txt_to_Func()
+        print(self.reader)
+        self.Save_to_file()
+
+
+
+
+def start_listener_click(reader):
+    with mouse.Listener(on_click=lambda x, y, b, p: reader.append(['on_click', x, y, b, p, time.perf_counter()])) as click_listener:
+        click_listener.join()
+def start_listener_move(reader):
+    with mouse.Listener(on_move=lambda x, y: reader.append(['on_move', x, y, time.perf_counter()])) as mouse_listener:
+        mouse_listener.join()
+def start_listener_keyboard(reader):
+    with keyboard.Listener(on_press=lambda key: reader.append(['on_press', key, time.perf_counter()]),
+                           on_release = lambda key: reader.append(['on_release', key, time.perf_counter()])) as keyboard_listener:
+        keyboard_listener.join()
 
 
 if __name__ == "__main__":
     with Manager() as manager:
         reader = manager.list()
-        p2 = Process(target=KeyboardEvent, args=(reader, time.time()))
-        p = Process(target=start_listener, args=(reader, time.time()))
-        p.start()
-        p2.start()
+
+        Pclick = Process(target=start_listener_click, args=(reader, ))
+        Pmove = Process(target=start_listener_move, args=(reader, ))
+        Pkeybroad = Process(target=start_listener_keyboard, args=(reader, ))
+
+        print('Для старта нажми ESC')
+        while True:
+            if kb.is_pressed('Esc'):
+                break
+        time.sleep(0.1) #Чтобы не было такого что ESC и запустил и закрыл
+        print('Начало записи')
+
+        time_start = time.perf_counter()
+        Pclick.start()
+        Pmove.start()
+        Pkeybroad.start()
 
         while True:
-            if keyboard.is_pressed('Esc'):
+            if kb.is_pressed('Esc'):
                 break
 
-        p.terminate()
-        p2.terminate()
+        Pclick.terminate()
+        Pmove.terminate()
+        Pkeybroad.terminate()
 
-        reader = list(reader[:])
-        reader2 = deepcopy(reader)
-        for i in range(1, len(reader)):
-            reader[i][-1] = reader[i][-1] - reader2[i - 1][-1]
+        print('Запись завершена')
 
-        # """Save to .pickle
-        with open("../readers/read_script.pickle", "wb") as file:
-            pickle.dump(reader, file)
-            file.close()
-        # """
+        save1 = Reformat(reader, time_start)
 
-        """Save to .txt
-        with open("../readers/read_script.txt", "w") as file:
-            for item in reader[:]:
-                file.write(str(item) + "\n")
-        """
+        print('Для воспроизведения нажми ESC')
+        while True:
+            if kb.is_pressed('Esc'):
+                break
+
+        with open('../readers/read_script.pickle', "rb") as file:
+            inputs = pickle.load(file)
+
+        for i in inputs:
+            i()
