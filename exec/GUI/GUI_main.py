06afc213a2c72c37a8e08fb3ab79c1fc496f37d2
OpexHunter <ivan.vekhov@mail.ru>
2023-11-08 20:42:32 +0300

    gui

--- exec/GUI/GUI_main.py
+++ exec/GUI/GUI_main.py
@@ -1,12 +1,23 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog
 from mainUI import Ui_MainWindow
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.sh_start.clicked.connect(self.open_file_dialog)
+
+    def open_file_dialog(self):
+        # Открываем диалог выбора файла
+        dialog = QFileDialog(self)
+        dialog.setDirectory("C:/Users/XUI/PycharmProjects/GInputAI_Main/exec")
+        dialog.setAcceptMode(QFileDialog.AcceptOpen)  # Режим выбора одного файла
+        dialog.fileSelected.connect(self.ui.file_path_label.setText)  # Подключаем слот для обновления QLabel
+        dialog.exec()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- exec/GUI/mainUI.py
+++ exec/GUI/mainUI.py
@@ -15,10 +15,10 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QCheckBox, QMainWindow, QPushButton,
-    QSizePolicy, QTabWidget, QTextBrowser, QTextEdit,
-    QVBoxLayout, QWidget)
-import Resources
+from PySide6.QtWidgets import (QApplication, QCheckBox, QLabel, QMainWindow,
+    QPushButton, QSizePolicy, QTabWidget, QVBoxLayout,
+    QWidget)
+import files_ico_rc
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -30,10 +30,10 @@ class Ui_MainWindow(object):
         MainWindow.setWindowIcon(icon)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.tabWidget = QTabWidget(self.centralwidget)
-        self.tabWidget.setObjectName(u"tabWidget")
-        self.tabWidget.setGeometry(QRect(0, 0, 781, 471))
-        self.tabWidget.setStyleSheet(u"QTabBar::tab {\n"
+        self.tab_main = QTabWidget(self.centralwidget)
+        self.tab_main.setObjectName(u"tab_main")
+        self.tab_main.setGeometry(QRect(0, 0, 781, 471))
+        self.tab_main.setStyleSheet(u"QTabBar::tab {\n"
 "    width: 120px; /* \u0428\u0438\u0440\u0438\u043d\u0430 \u0432\u043a\u043b\u0430\u0434\u043a\u0438 */\n"
 "    height: 30px; /* \u0412\u044b\u0441\u043e\u0442\u0430 \u0432\u043a\u043b\u0430\u0434\u043a\u0438 */\n"
 "    padding: 5px; /* \u041e\u0442\u0441\u0442\u0443\u043f\u044b \u0432\u043d\u0443\u0442\u0440\u0438 \u0432\u043a\u043b\u0430\u0434\u043a\u0438 */\n"
@@ -44,31 +44,18 @@ class Ui_MainWindow(object):
 "    align-self: center;\n"
 "}\n"
 "")
-        self.tab = QWidget()
-        self.tab.setObjectName(u"tab")
-        self.tab.setStyleSheet(u"")
-        self.textBrowser = QTextBrowser(self.tab)
-        self.textBrowser.setObjectName(u"textBrowser")
-        self.textBrowser.setGeometry(QRect(10, 10, 661, 371))
-        self.tabWidget.addTab(self.tab, "")
-        self.tab_2 = QWidget()
-        self.tab_2.setObjectName(u"tab_2")
-        self.textEdit = QTextEdit(self.tab_2)
-        self.textEdit.setObjectName(u"textEdit")
-        self.textEdit.setGeometry(QRect(10, 10, 761, 351))
-        self.pushButton = QPushButton(self.tab_2)
-        self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(400, 370, 161, 41))
-        self.pushButton_2 = QPushButton(self.tab_2)
-        self.pushButton_2.setObjectName(u"pushButton_2")
-        self.pushButton_2.setGeometry(QRect(10, 370, 321, 41))
-        self.pushButton_3 = QPushButton(self.tab_2)
-        self.pushButton_3.setObjectName(u"pushButton_3")
-        self.pushButton_3.setGeometry(QRect(570, 370, 201, 41))
-        self.tabWidget.addTab(self.tab_2, "")
-        self.tab_3 = QWidget()
-        self.tab_3.setObjectName(u"tab_3")
-        self.verticalLayoutWidget = QWidget(self.tab_3)
+        self.tab1 = QWidget()
+        self.tab1.setObjectName(u"tab1")
+        self.sh_start = QPushButton(self.tab1)
+        self.sh_start.setObjectName(u"sh_start")
+        self.sh_start.setGeometry(QRect(570, 370, 201, 41))
+        self.file_path_label = QLabel(self.tab1)
+        self.file_path_label.setObjectName(u"file_path_label")
+        self.file_path_label.setGeometry(QRect(90, 380, 441, 20))
+        self.tab_main.addTab(self.tab1, "")
+        self.tab2 = QWidget()
+        self.tab2.setObjectName(u"tab2")
+        self.verticalLayoutWidget = QWidget(self.tab2)
         self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
         self.verticalLayoutWidget.setGeometry(QRect(10, 10, 162, 103))
         self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
@@ -94,21 +81,21 @@ class Ui_MainWindow(object):
 
         self.verticalLayout.addWidget(self.checkBox_4)
 
-        self.tabWidget.addTab(self.tab_3, "")
-        self.tab_5 = QWidget()
-        self.tab_5.setObjectName(u"tab_5")
-        self.tabWidget.addTab(self.tab_5, "")
-        self.tab_4 = QWidget()
-        self.tab_4.setObjectName(u"tab_4")
-        self.tabWidget.addTab(self.tab_4, "")
-        self.tab_6 = QWidget()
-        self.tab_6.setObjectName(u"tab_6")
-        self.tabWidget.addTab(self.tab_6, "")
+        self.tab_main.addTab(self.tab2, "")
+        self.tab4 = QWidget()
+        self.tab4.setObjectName(u"tab4")
+        self.tab_main.addTab(self.tab4, "")
+        self.tab3 = QWidget()
+        self.tab3.setObjectName(u"tab3")
+        self.tab_main.addTab(self.tab3, "")
+        self.tab5 = QWidget()
+        self.tab5.setObjectName(u"tab5")
+        self.tab_main.addTab(self.tab5, "")
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
 
-        self.tabWidget.setCurrentIndex(0)
+        self.tab_main.setCurrentIndex(0)
 
 
         QMetaObject.connectSlotsByName(MainWindow)
@@ -116,23 +103,16 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"GInput AI", None))
-        self.textBrowser.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:'Segoe UI'; font-size:16pt; font-weight:696;\">\u041f\u0440\u043e \u043d\u0430\u0441 \u043a\u043e\u0440\u043e\u0447\u0435 \u043c\u044b \u043b\u044e\u0431\u0438\u043c \u0447\u043b\u0435\u043d\u044b \u043d\u0443 \u0442\u0438\u043f\u0430 \u0442\u043e\u0433\u043e</span></p></body></html>", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("MainWindow", u"Main menu", None))
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Stop", None))
-        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"Insert Helper", None))
-        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"Start", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("MainWindow", u"Script Handler", None))
+        self.sh_start.setText(QCoreApplication.translate("MainWindow", u"Start", None))
+        self.file_path_label.setText(QCoreApplication.translate("MainWindow", u"MIIAAYY", None))
+        self.tab_main.setTabText(self.tab_main.indexOf(self.tab1), QCoreApplication.translate("MainWindow", u"Script Handler", None))
         self.checkBox.setText(QCoreApplication.translate("MainWindow", u"\u041a\u043b\u0430\u0432\u0438\u0430\u0442\u0443\u0440\u0430", None))
         self.checkBox_3.setText(QCoreApplication.translate("MainWindow", u"\u041c\u044b\u0448\u044c (\u041a\u043b\u0438\u043a\u0438)", None))
         self.checkBox_2.setText(QCoreApplication.translate("MainWindow", u"\u041c\u044b\u0448\u044c (\u0412\u0441\u0435 \u0434\u0432\u0438\u0436\u0435\u043d\u0438\u044f)", None))
         self.checkBox_4.setText(QCoreApplication.translate("MainWindow", u"\u0421\u043a\u0440\u043e\u043b\u043b", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), QCoreApplication.translate("MainWindow", u"Script Recorder", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_5), QCoreApplication.translate("MainWindow", u"AI Data/Learn", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_4), QCoreApplication.translate("MainWindow", u"Settings", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_6), QCoreApplication.translate("MainWindow", u"Profile", None))
+        self.tab_main.setTabText(self.tab_main.indexOf(self.tab2), QCoreApplication.translate("MainWindow", u"Script Recorder", None))
+        self.tab_main.setTabText(self.tab_main.indexOf(self.tab4), QCoreApplication.translate("MainWindow", u"AI Data/Learn", None))
+        self.tab_main.setTabText(self.tab_main.indexOf(self.tab3), QCoreApplication.translate("MainWindow", u"Settings", None))
+        self.tab_main.setTabText(self.tab_main.indexOf(self.tab5), QCoreApplication.translate("MainWindow", u"Profile", None))
     # retranslateUi
 
